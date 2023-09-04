c98ca24e657a46fcd3ccdedd4f54c0567d53be94
Vitor Juanh <vitor.juanh@gmail.com>
2023-09-03 22:00:26 -0400

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+import CreateCourseService from './CreateCourseService';
+
+export function createCourse(request: Request, response: Response) {
+    CreateCourseService.execute({
+        name: "NodeJS",
+        duration: 10,
+        educator: "Vitor"
+    })
+}
